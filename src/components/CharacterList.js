5a52bea16331392791ae2822dcab42c2cbf1f003
biskoi <biskoi@daydream.cafe>
2020-02-28 10:19:18 -0700

    search bar functional

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,30 +2,31 @@ import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import SearchForm from './SearchForm';
 import CharacterCard from './CharacterCard';
-import data from './dummydata';
+import {Link, useParams} from 'react-router-dom';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
 
-  const [characters, setCharacters] = useState();
+  const [characters, setCharacters] = useState([]);
   const [query, setQuery] = useState('');
 
-  // useEffect(() => {
-  //   // TODO: Add API Request here - must run in `useEffect`
-  //   //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+  useEffect(() => {
+    // TODO: Add API Request here - must run in `useEffect`
+    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
 
-  //   axios.get('https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character').then(res => {
-  //     setCharacters(res.data.results);
-  //   }).catch(err => console.log(err))
-  // }, []);
+    axios.get('https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character').then(res => {
+      const results = res.data.results.filter(item => item.name.toLowerCase().includes(query.toLowerCase()));
+      setCharacters(results);
+    }).catch(err => console.log(err))
+  }, [query]);
 
   console.log(characters);
 
   return (
     <section className="character-list">
-      <SearchForm/>
-      {data.map(item => (
-        <CharacterCard char = {item}/>
+      <SearchForm setQuery = {setQuery} query = {query}/>
+      {characters.map(item => (
+      <CharacterCard char = {item}/>
       ))}
     </section>
   );
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,39 @@
 import React, { useState } from "react";
+import {withFormik, Field, Form} from 'formik';
 
-export default function SearchForm() {
+function SearchForm(props) {
  
   return (
     <section className="search-form">
-     // Add a search form here
+
+    <Form>
+      <label htmlFor = 'search'>
+        Search
+        <Field name = 'search' placeholder = 'Enter a search term'/>
+      </label>
+    </Form>
+
     </section>
   );
+
 }
+
+const FormikSearch = withFormik({
+
+  mapPropsToValues: props => {
+
+    return {
+      search: props.query || ''
+    }
+
+  },
+
+  handleSubmit: (values, bag) => {
+    console.log(values, bag);
+    bag.props.setQuery(values.search);
+  }
+
+
+})(SearchForm);
+
+export default FormikSearch;
